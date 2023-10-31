23a59fa4759490df623a2988792a04995e851876
Alberto Aparicio <alapvi@yahoo.com>
2023-10-31 10:33:16 +0100

    Student v1

---
+++ studentmain.py
@@ -0,0 +1,39 @@
+import studentcontroller as sc
+controllerStudent = sc.StudentController()
+
+while True:
+    print("STUDENT CRUD")
+    print("____________")
+    print("1.- Add student")
+    print("2.- Delete student")
+    print("3.- Modify student")
+    print("4.- Search student")
+    print("5.- Exit")
+    option = int(input("Choose option:"))
+    if (option == 5):
+        break
+    elif (option == 1):
+        print("Adding a new Student!")
+        dni = input("DNI:")
+        name = input("Name:")
+        surname = input("Surname:")
+        age = int(input("Age:"))
+        city = input("City:")
+        province = input("Province:")
+        email = input("Email:")
+        if controllerStudent.addStudent(dni,name,surname,age,city,province,email):
+            print("Student added!!")
+        else:
+            print("DNI exist!")
+    elif (option == 2):
+        print("Deleting a Student!")
+        dni = input("DNI:")
+        student = controllerStudent.delStudent(dni)
+        if student != None:
+            print("Student deleted!",student.getDni())
+        else:
+            print("Student by DNI not found!")
+        
+
+print("Bye!")
+    
